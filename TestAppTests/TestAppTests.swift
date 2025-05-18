f0f3e0bd7efcdd0e99b6dd25e6af1c877af67e05
s_kir <syskirill1@gmail.com>
2025-05-18 16:44:54 +0300

    Initial Commit

---
+++ TestAppTests/TestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppTests.swift
+//  TestAppTests
+//
+//  Created by Kirill Sysoev on 18.05.2025.
+//
+
+import Testing
+@testable import TestApp
+
+struct TestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
